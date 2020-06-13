d8f9ca3d0ce1892caa693a3c2b06e360a59d0f39
Keketso Ramosedi <Keketsor@astel.co.za>
2020-06-13 20:51:14 +0200

    Project Structure and Shell Navigation Setup

---
+++ MinoxBeard/MinoxBeard/Helpers/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+
+namespace MinoxBeard.Helpers
+{
+    public static class ViewModelLocator
+    {
+
+        //public static WebapiViewModel WebapiViewModel => ServiceLocator.Current.GetInstance<WebapiViewModel>();
+        
+
+    }
+}
