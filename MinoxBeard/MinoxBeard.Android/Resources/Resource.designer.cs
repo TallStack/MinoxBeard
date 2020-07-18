10c9b4adf2b1209e22a165fae3edfd87486a73a5
Keketso Ramosedi <Keketsor@astel.co.za>
2020-07-19 01:50:26 +0200

    Routine page now partially functional

--- MinoxBeard/MinoxBeard.Android/Resources/Resource.designer.cs
+++ MinoxBeard/MinoxBeard.Android/Resources/Resource.designer.cs
@@ -11007,19 +11007,22 @@ namespace MinoxBeard.Droid
 			public const int notify_panel_notification_icon_bg = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int routine = 2131165319;
+			public const int overlay = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int splashscreenads = 2131165320;
+			public const int routine = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int tooltip_frame_dark = 2131165321;
+			public const int splashscreenads = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int tooltip_frame_light = 2131165322;
+			public const int tooltip_frame_dark = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int wiki = 2131165323;
+			public const int tooltip_frame_light = 2131165323;
+			
+			// aapt resource value: 0x7F07008C
+			public const int wiki = 2131165324;
 			
 			static Drawable()
 			{
--- MinoxBeard/MinoxBeard/Views/RoutinePage.xaml.cs
+++ MinoxBeard/MinoxBeard/Views/RoutinePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MinoxBeard.Controls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +17,49 @@ namespace MinoxBeard.Views
         public RoutinePage()
         {
             InitializeComponent();
+            this.BindingContext = this;
         }
 
-        async void Button_Clicked(object sender, EventArgs e)
+        public ObservableCollection<Agenda> MyAgenda { get => GetAgenda(); }
+
+        private ObservableCollection<Agenda> GetAgenda()
         {
-            await DependencyService.Get<IAdmobInterstitialAds>().Display(AppConstants.InterstitialAdId);
+            return new ObservableCollection<Agenda>
+            {
+                new Agenda { Topic = "All Things Xamarin", Duration = "07:30 UTC - 11:30 UTC", Color = "#B96CBD", Date = new DateTime(2020, 3, 23),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } },
+
+                new Agenda { Topic = "Visualize Your Data", Duration = "07:30 UTC - 11:30 UTC", Color = "#49A24D", Date = new DateTime(2020, 3, 24),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } },
+
+                new Agenda { Topic = "Testing Your Xamarin Apps", Duration = "07:30 UTC - 11:30 UTC", Color = "#FDA838", Date = new DateTime(2020, 3, 25),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } },
+
+                new Agenda { Topic = "Xamarin Productivity to the Max", Duration = "07:30 UTC - 11:30 UTC", Color = "#F75355", Date = new DateTime(2020, 3, 26),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } },
+
+                new Agenda { Topic = "All Things Xamarin.Forms Shell", Duration = "07:30 UTC - 11:30 UTC", Color = "#00C6AE", Date = new DateTime(2020, 3, 27),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } },
+
+                new Agenda { Topic = "Building Beautiful Apps", Duration = "07:30 UTC - 11:30 UTC", Color = "#455399", Date = new DateTime(2020, 3, 28),
+                    Speakers = new ObservableCollection<Speaker>{ new Speaker { Name = "Maddy Leger", Time = "07:30" }, new Speaker { Name = "David Ortinau", Time = "08:30" }, new Speaker { Name = "Gerald Versluis", Time = "10:30" } } }
+            };
         }
+    }
+
+
+    public class Agenda
+    {
+        public string Topic { get; set; }
+        public string Duration { get; set; }
+        public DateTime Date { get; set; }
+        public ObservableCollection<Speaker> Speakers { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class Speaker
+    {
+        public string Name { get; set; }
+        public string Time { get; set; }
     }
 }
